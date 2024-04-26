bb3641d2cbd9acd9425143ecfed199635cc95096
amoeba04 <amoeba04@gmail.com>
2024-04-26 12:15:41 +0900

    Minor modifications

--- train_v1.1b/train_rtx3090_3x.sh
+++ train_v1.1b/train_rtx3090_3x.sh
@@ -19,18 +19,20 @@
 
 CUDA_VISIBLE_DEVICES=1,2,3,4 python run_clm.py \
 --model_name_or_path='yanolja/EEVE-Korean-Instruct-10.8B-v1.0' \
---train_file='./KoAlpaca_v1.1a_textonly.json' \
+--train_file='./Korean_Personal_Information_Sentences_edit.csv' \
 --num_train_epochs=1 \
 --block_size=1024 \
 --per_device_train_batch_size=1 \
 --gradient_accumulation_steps=8 \
 --fp16 \
---output_dir='eeve-10.8b-koalpaca-v1.1a-rtx3090' \
+--output_dir='eeve-10.8b-privacy-sentence' \
 --do_train \
 --optim='adafactor' \
 --learning_rate='2e-5' \
 --logging_strategy='steps' \
 --logging_first_step \
---run_name='eeve-10.8b-koalpaca-v1.1a-rtx3090' \
+--run_name='eeve-10.8b-privacy-sentence' \
 --low_cpu_mem_usage \
---overwrite_output_dir
+--overwrite_output_dir \
+--save_steps 200 \
+--save_total_limit 1
