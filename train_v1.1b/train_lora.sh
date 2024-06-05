c00f29ae2a797c831567f584830ae5d2ec9e5a2f
amoeba04 <amoeba04@gmail.com>
2024-06-05 16:00:55 +0900

    Update LoRA training

---
+++ train_v1.1b/train_lora.sh
@@ -0,0 +1,17 @@
+CUDA_VISIBLE_DEVICES=6,7 python run_lora.py \
+--model_name_or_path='yanolja/EEVE-Korean-Instruct-10.8B-v1.0' \
+--train_file='./Merged_Instruction_eeve1000.csv' \
+--num_train_epochs=3 \
+--block_size=1024 \
+--per_device_train_batch_size=1 \
+--gradient_accumulation_steps=8 \
+--fp16 \
+--output_dir='eeve-lora-privacy-merged1000' \
+--do_train \
+--optim='adafactor' \
+--learning_rate='2e-5' \
+--logging_strategy='steps' \
+--logging_first_step \
+--low_cpu_mem_usage \
+--overwrite_output_dir \
+--save_strategy='epoch'
