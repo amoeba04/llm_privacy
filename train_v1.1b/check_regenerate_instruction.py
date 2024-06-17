cd7d7baa51679fa1b1443ec1402523b23acb38af
amoeba04 <amoeba04@gmail.com>
2024-06-17 17:09:53 +0900

    Minor updates

--- train_v1.1b/check_regenerate_instruction.py
+++ train_v1.1b/check_regenerate_instruction.py
@@ -1,18 +1,50 @@
 import pandas as pd
+import torch
+from safetensors import safe_open
 from transformers import pipeline, AutoModelForCausalLM, AutoTokenizer
 
-MODEL = "eeve-privacy-merged"
-MODEL = "llama3-privacy-merged"
+def fix_state_dict(state_dict):
+    # _module 접두어가 붙은 키를 찾아서 접두어를 제거
+    fixed_state_dict = {key.replace('_module.', ''): value for key, value in state_dict.items() if '_module.' in key}
+    # 원본 state_dict에서 _module 접두어가 없는 키도 추가
+    fixed_state_dict.update({key: value for key, value in state_dict.items() if '_module.' not in key})
+    return fixed_state_dict
 
-file_path = 'Korean_Personal_Instruction_eeve_selected.csv'
-file_path = 'Korean_Personal_Instruction_llama3_selected.csv'
+# MODEL = "eeve-privacy-merged1000/checkpoint-3780"
+# MODEL = "llama3-privacy-merged"
+# MODEL = "openelm-dp-privacy-merged1000/checkpoint-63530"
+MODEL = "jungyuko/DAVinCI-42dot_LLM-PLM-1.3B-v1.2"
+
+# file_path = 'Korean_Personal_Instruction_eeve_selected1000.csv'
+# file_path = 'Korean_Personal_Instruction_llama3_selected.csv'
+# file_path = 'Korean_Personal_Instruction_openelm_selected1000.csv'
+file_path = 'Korean_Personal_Instruction_davinci_selected1000.csv'
 
 tokenizer = AutoTokenizer.from_pretrained(MODEL)
-model = AutoModelForCausalLM.from_pretrained(
-    MODEL,
-    torch_dtype="auto",
-    device_map="auto",
-)
+
+if 'dp' in MODEL:
+    tensors = {}
+    with safe_open(f'{MODEL}/model.safetensors', framework="pt", device="cpu") as f:
+        for key in f.keys():
+            tensors[key] = f.get_tensor(key)
+
+    # state_dict에서 파라미터 이름 수정
+    fixed_state_dict = fix_state_dict(tensors)
+
+    # 새로운 모델 인스턴스 생성
+    model = AutoModelForCausalLM.from_pretrained(MODEL, 
+                                                 torch_dtype="auto", 
+                                                 device_map="auto",
+                                                 )
+
+    # 수정된 state_dict를 새 모델 인스턴스에 적용
+    model.load_state_dict(fixed_state_dict)
+else:
+    model = AutoModelForCausalLM.from_pretrained(
+        MODEL,
+        torch_dtype="auto",
+        device_map="auto",
+    )
 
 pipe = pipeline(
     "text-generation",
@@ -21,7 +53,8 @@ pipe = pipeline(
 )
 terminators = [
     tokenizer.eos_token_id,
-    tokenizer.convert_tokens_to_ids("<|eot_id|>")
+    tokenizer.convert_tokens_to_ids("<|eot_id|>"),
+    tokenizer.convert_tokens_to_ids("</s>")
 ]
 
 with open(file_path, 'r') as file:
@@ -63,11 +96,12 @@ for index, row in data.iterrows():
         top_p=0.9,
         return_full_text=False,
         eos_token_id=terminators,
-        repetition_penalty=1.2,
+        repetition_penalty=1.5,
         )[0]['generated_text']
     
     # 문장 비교
-    if generated_text in assistant_gt:
+    print('\n 입력 질문:', inputs, '\n\n 튜닝전모델 답변:', generated_text)
+    if generated_text in assistant_gt or assistant_gt in generated_text:
         correct_count += 1
         print(f'Memorized: {user}{assistant_gt}')
     
@@ -87,6 +121,9 @@ print(f"# of memorized data: {correct_count}")
 print(f"Ratio of memorized data: {accuracy:.2f}")
 
 # 최종 결과를 CSV 파일로 저장
-output_path = 'Generated_' + file_path
+if '1000' in file_path:
+    output_path = 'Generated_1000_1ep_DP_' + file_path
+else:
+    output_path = 'Generated_' + file_path
 data.to_csv(output_path, index=False)
 print(f"Data saved to {output_path}")
